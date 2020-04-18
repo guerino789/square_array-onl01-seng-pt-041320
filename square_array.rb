ad5b01a0632684d67ac08c2ae638950fa815db9e
guerino antonini <guerantonini94@gmail.com>
2020-04-18 21:15:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   squared_array = [1,2,3]
   array.each do |num|
     square = num ** 2 
-    squared_array << array
+    squared_array << square
    end
-   array
+   squared_array
 end
